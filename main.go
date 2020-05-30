c1afc4f5da80e2e08b83960f5b016bff010a0c0e
ivan-bogach <catmrgerman@gmail.com>
2020-05-30 15:27:08 +0300

    ws done

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
-
-	"time"
+	"strings"
 
 	"github.com/gorilla/websocket"
+	"github.com/huin/goserial"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
@@ -16,11 +19,8 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-func write(msg chan *Message) {
-	for {
-		time.Sleep(5 * time.Second)
-		msg <- &Message{Message: "AAAAAAAAAAAAAAAAAA"}
-	}
+func write(msg chan *Message, str string) {
+	msg <- &Message{Message: str}
 }
 
 func main() {
@@ -28,11 +28,77 @@ func main() {
 
 	hub := newHub()
 	go hub.run(msg)
-
+	var prevAMsg string
 	go func() {
+
+		buf := make([]byte, 16)
 		d := &Device{hub: hub, send: make(chan []byte), localAddr: "loc"}
 		d.hub.reg <- d
-		go write(msg)
+
+		gays := make(map[string]string)
+		gays["342170325308233"] = "Pashtet"
+		gays["315119351158105"] = "Selo"
+
+		c := &goserial.Config{Name: findArduino(), Baud: 115200}
+		s, err := goserial.OpenPort(c)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			n, err := io.ReadFull(s, buf)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			aMsg := string(buf[:n])
+			fmt.Println(prevAMsg + "<===>" + aMsg)
+			fmt.Println(prevAMsg == aMsg)
+			if prevAMsg == aMsg {
+				fmt.Println("AAAAAAAAAAAAAAAAAAAAAAAAAAA")
+				continue
+			}
+			prevAMsg = aMsg
+			btns := strings.Split(aMsg, "")[0]
+			lastCardID := strings.Split(aMsg, "")[1:]
+			fmt.Printf("%s\n", aMsg)
+			switch btns {
+			case "1":
+				fmt.Println("Opened door")
+				go write(msg, "Opened door")
+				_, err := s.Write([]byte("0"))
+				if err != nil {
+					log.Fatal(err)
+				}
+			case "2":
+				go write(msg, "Ring")
+				fmt.Println("Ring")
+			case "3":
+				go write(msg, "Opened and Ring")
+				fmt.Println("Opened and Ring")
+			case "4":
+				go write(msg, "Attention!")
+				fmt.Println("Attention!")
+			case "5":
+				go write(msg, "Opened and 5")
+				fmt.Println("Opened door & chtoto")
+			case "6":
+				go write(msg, "Opened 6")
+				fmt.Println("Code5")
+			case "7":
+				fmt.Println("Together")
+			}
+
+			fmt.Println("Last time come in: %s", gays[strings.Join(lastCardID, "")])
+			if _, ok := gays[strings.Join(lastCardID, "")]; ok {
+				n, err := s.Write([]byte("1"))
+				fmt.Println("wroten %d bytes", n)
+				go write(msg, gays[strings.Join(lastCardID, "")])
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
 	}()
 
 	flag.Parse()
@@ -44,4 +110,20 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func findArduino() string {
+	contents, _ := ioutil.ReadDir("/dev")
+
+	// Look for what is mostly likely the Arduino device
+	for _, f := range contents {
+		if strings.Contains(f.Name(), "tty.usbserial") ||
+			strings.Contains(f.Name(), "ttyACM0") {
+			return "/dev/" + f.Name()
+		}
+	}
+
+	// Have not been able to find a USB device that 'looks'
+	// like an Arduino.
+	return ""
 }
